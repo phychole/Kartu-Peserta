8197249b3bdb532543f75ab3597b8bc284355779
MUHAMAD FAISOL <145274220+phychole@users.noreply.github.com>
2026-02-28 23:48:36 -0800

    Update database configuration for application

--- config/app.php
+++ config/app.php
@@ -2,9 +2,9 @@
 // === KONFIGURASI APLIKASI ===
 // Sesuaikan dengan database MySQL Anda (Laragon biasanya: host=localhost, user=root, pass kosong)
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'psaj');
+define('DB_NAME', 'psaj_kartu_ujian');
 define('DB_USER', 'root');
-define('DB_PASS', 'ic4lsmkn2');
+define('DB_PASS', '');
 
 //tahun pelajaran
 define('TAHUN_PELAJARAN', '2025 - 2026');
